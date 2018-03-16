a1b618d55d21aa4bd411f69b209d2ec4830cafa5
Ryan McDougall <rp.mcdougall@gmail.com>
2018-03-16 17:32:04 -0500

    works on ratpack-groovy 1.3.3

--- build/resources/main/ratpack.groovy
+++ build/resources/main/ratpack.groovy
@@ -1,10 +1,21 @@
+import ratpack.hikari.HikariModule
+
 import static ratpack.groovy.Groovy.ratpack
 
 ratpack {
-    handlers {
-        all(new CORSHandler())
-        get {
-            render "It's wednesday. my dudes."
+    bindings {
+        module(HikariModule) { config ->
+            config.dataSourceClassName = 'org.h2.jdbcx.JdbcDataSource'
+            config.addDataSourceProperty('URL', "jdbc:h2:mem:tood;INIT=RUNSCRIPT FROM 'classpath:/init.sql'")
         }
+        module(TodoModule)
+        bindInstance(new CORSHandler())
+        bindInstance(new TodoBaseHandler())
+        bindInstance(new TodoChain())
+        bindInstance(new TodoHandler())
+    }
+    handlers {
+        all(CORSHandler)
+        insert(TodoChain)
     }
 }
